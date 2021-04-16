6995badc82eddff8798b30b6c31361398e5a3a19
Karan <53494842+krndxs@users.noreply.github.com>
2021-04-16 13:46:23 -0700

    Added Login Activity (1st commit didn't work for some reason...)

---
+++ app/src/main/java/com/gupta/cst438_s21_groupa_proj3/Login.java
@@ -0,0 +1,14 @@
+package com.gupta.cst438_s21_groupa_proj3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
