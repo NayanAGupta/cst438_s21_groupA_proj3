764f7dce35c455330fc302d2116f9a1c8b90addc
Justin Le <jule@csumb.edu>
2021-04-16 17:45:31 -0700

    added sign in and login framework and parse dependencies

--- app/src/main/java/com/gupta/cst438_s21_groupa_proj3/MainActivity.java
+++ app/src/main/java/com/gupta/cst438_s21_groupa_proj3/MainActivity.java
@@ -2,16 +2,39 @@ package com.gupta.cst438_s21_groupa_proj3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+import com.parse.Parse;
+import com.parse.SignUpCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button signUpButton;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        Button signUpButton = findViewById(R.id.signUp);
+        Button loginButton = findViewById(R.id.login);
+
+        signUpButton.setOnClickListener(v1->{
+            Intent intent = new Intent(this.getApplicationContext(), CreateAccountActivity.class);
+            startActivity(intent);
+        });
+
     }
+
 }
