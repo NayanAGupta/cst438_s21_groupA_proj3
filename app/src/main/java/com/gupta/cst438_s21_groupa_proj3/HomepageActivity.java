a4ab686e2e8c370bbf6ceeeb39ac191f9b7de395
NAYAN GUPTA <ngupta@csumb.edu>
2021-04-16 22:54:28 -0700

    Created settings menu

--- app/src/main/java/com/gupta/cst438_s21_groupa_proj3/HomepageActivity.java
+++ app/src/main/java/com/gupta/cst438_s21_groupa_proj3/HomepageActivity.java
@@ -1,9 +1,14 @@
 package com.gupta.cst438_s21_groupa_proj3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,16 +17,21 @@ import android.widget.Toast;
 import com.parse.ParseUser;
 
 public class HomepageActivity extends AppCompatActivity {
-    Button logoutButton;
+
+    Toolbar toolbar;
     TextView welcomeTextView;
+    Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-        logoutButton = findViewById(R.id.logoutButton);
+        toolbar = findViewById(R.id.toolbar);
         welcomeTextView = findViewById(R.id.welcomeTextView);
+        logoutButton = findViewById(R.id.logoutButton);
+
+        setSupportActionBar(toolbar);
 
         String welcomeMessage = "Welcome, "+ ParseUser.getCurrentUser().getUsername() + "!";
         welcomeTextView.setText(welcomeMessage);
@@ -34,5 +44,25 @@ public class HomepageActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_menu, menu);
+        return true;
+
+
     }
 }
