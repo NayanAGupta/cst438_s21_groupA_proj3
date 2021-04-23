cdc1f5f2b66cfa604f6630a3c3ffa5eb5acc012e
Justin Le <jule@csumb.edu>
2021-04-22 17:05:45 -0700

    get current user's book get list of recipes from current user's book

--- app/src/main/java/com/gupta/cst438_s21_groupa_proj3/Submit.java
+++ app/src/main/java/com/gupta/cst438_s21_groupa_proj3/Submit.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.GetCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Submit extends AppCompatActivity {
 
@@ -20,6 +27,9 @@ public class Submit extends AppCompatActivity {
     EditText recipeDescription;
     EditText recipeImage;
     Button recipeSubmit;
+
+    ArrayList<String> recipeBookList= new ArrayList<String>();
+    String currUserBookId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +51,36 @@ public class Submit extends AppCompatActivity {
                 else{
                     ParseObject recipe = new ParseObject("recipe");
                     recipe.put("name",recipeName.getText().toString());
-                    recipe.addAllUnique("ingredientIDList", Arrays.asList(recipeList.getText().toString()));
+                    recipe.addAllUnique("ingredientIDList", Arrays.asList(recipeList.getText().toString())); //comma seperate with no spaces ex: ingredient1,ingredient2,
                     recipe.put("description",recipeDescription.getText().toString());
                     recipe.put("imageURL",recipeImage.getText().toString());
                     recipe.saveInBackground();
-                    Toast.makeText(getApplicationContext(),"Submission Successful",Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(getApplicationContext(), HomepageActivity.class);
-                    startActivity(intent);
+
+                    //get current user's recipebook
+                    ParseUser currentUser = ParseUser.getCurrentUser();
+                    currUserBookId = currentUser.getString("recipeBookId");
+                    ParseQuery<ParseObject> query = ParseQuery.getQuery("recipeBook");
+                    query.getInBackground(currUserBookId, new GetCallback<ParseObject>() {
+                        @Override
+                        public void done(ParseObject object, ParseException e) {
+                            if (e == null){
+                                //found book
+                                recipeBookList.addAll(object.getList("recipeIDList"));
+                                String title = object.getString("recipeBookTitle");
+                                Log.d("book", "Found " + title);
+                                String temp = new String();
+                                for(String entry : recipeBookList){
+                                    temp += entry;
+                                }
+                                Log.d("book", "Content: " + temp);
+                                Toast.makeText(getApplicationContext(),"Submission Successful",Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(getApplicationContext(), HomepageActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(getApplicationContext(),"Error querying for recipe book: "+e.getMessage(),Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
                 }
             }
         });
